d365f34a6f4ea0ec8cf5a58edff2767cba190195
Marek Rewers <mrewers@users.noreply.github.com>
2018-09-23 13:21:50 -0400

    Add classes and some styling

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
   <footer>
-    <p>Footer</p>
-    <?php wp_nav_menu( array('theme_location'=>'footer_nav') ); ?>
+    <div class="footer-content">
+      <p>Footer</p>
+      <?php wp_nav_menu( array('theme_location'=>'footer_nav') ); ?>
+    </div> <!-- End footer-content -->
   </footer>
 
   <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php get_header(); ?>
 
 <div class="wrap">
-  <h1>Pianosa Test</h1>
 
   <?php if( have_posts() ):
 
     while( have_posts() ): the_post(); ?>
 
-      <h2><?php the_title(); ?></h2>
-      <small><?php the_author(); ?> | <?php the_date(); ?></small>
-      <article><?php the_content(); ?></article>
-      <small><?php the_category(); ?></small>
+      <h2 class="page-title"><?php the_title(); ?></h2>
+      <small class="page-byline"><?php the_author(); ?> | <?php the_date(); ?></small>
+      <article class="page-content"><?php the_content(); ?></article>
+      <small class="page-categories"><?php the_category(); ?></small>
       <hr />
   
     <?php endwhile;
