e93ec517bab2cc3f5522a9843238423028a5159f
Marek Rewers <mrewers@users.noreply.github.com>
2018-10-14 22:54:03 -0400

    Consolidate taxonomy link styles

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,5 +13,10 @@
   <h2 class="post-title"><?php the_title(); ?></h2>
   <small class="post-byline"><?php the_author(); ?> | <?php echo get_the_date(); ?></small>
   <div class="post-content"><?php the_content(); ?></div>
-  <small class="post-category"><?php the_category(); ?></small>
+  <small class="post-category"><?php the_category(); the_tags( '<ul class="post-tags"><li>', '</li><li>', '</li></ul>' )?></small>
+
+  <?php if( comments_open() ) {
+    comments_template();
+  } ?>
+
 </article>
